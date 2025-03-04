2bcb2e3900c70bd64d9b0145a2c5ca4df4bf4454
jensr <jensrodin16@gmail.com>
2025-03-04 12:07:53 +0100

    Kitchen Update

--- app/src/main/java/com/dt170g/kitchen/KitchenActivity.java
+++ app/src/main/java/com/dt170g/kitchen/KitchenActivity.java
@@ -3,13 +3,17 @@ package com.dt170g.kitchen;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.dt170g.api.ApiService;
 import com.dt170g.api.RetrofitClient;
 import com.google.gson.Gson;
+
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -18,7 +22,7 @@ public class KitchenActivity extends AppCompatActivity implements OrderAdapter.O
 
     private RecyclerView recyclerView;
     private OrderAdapter orderAdapter;
-    private List<RecievedOrder> orderList;
+    private List<RecievedOrder> orderList;  // The list of orders from the DB
     private ApiService apiService;
 
     @Override
@@ -26,13 +30,14 @@ public class KitchenActivity extends AppCompatActivity implements OrderAdapter.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kitchen_screen);
 
-        recyclerView = findViewById(R.id.recyclerViewReadyOrders);
+        // 1) Setup the RecyclerView
+        recyclerView = findViewById(R.id.recyclerViewOrders);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-
+        // 2) Get the API service
         apiService = RetrofitClient.getInstance().getApi();
 
-
+        // 3) Fetch orders right away
         fetchActiveOrders();
     }
 
@@ -42,9 +47,13 @@ public class KitchenActivity extends AppCompatActivity implements OrderAdapter.O
             public void onResponse(Call<List<RecievedOrder>> call, Response<List<RecievedOrder>> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     orderList = response.body();
+
+                    // Create and attach our adapter
                     orderAdapter = new OrderAdapter(orderList, KitchenActivity.this);
                     recyclerView.setAdapter(orderAdapter);
-                    Log.d("API_RESPONSE", new Gson().toJson(orderList));
+
+                    // Debug log the JSON we received
+                    Log.d("API_RESPONSE", "Orders from DB: " + new Gson().toJson(orderList));
                 } else {
                     Toast.makeText(KitchenActivity.this, "No orders found", Toast.LENGTH_SHORT).show();
                 }
@@ -57,27 +66,22 @@ public class KitchenActivity extends AppCompatActivity implements OrderAdapter.O
         });
     }
 
-
-
+    /**
+     * Called when the adapter's “Markera Order Som Klar” button is clicked.
+     * For now, we just display a Toast to prove we got the click.
+     */
     @Override
     public void onOrderReady(RecievedOrder order, Boolean starter, Boolean main, Boolean dessert) {
-        Toast.makeText(this, "Maträtt för bord Nr " + order.getTableNumber() + " är klar!", Toast.LENGTH_SHORT).show();
-        sendSignalToPersonalApp(order.getTableNumber(), starter, main, dessert);
-        orderAdapter.notifyDataSetChanged();
-    }
+        String msg = "Bord " + order.getTableNumber()
+                + ": Förrätt=" + starter
+                + ", Huvudrätt=" + main
+                + ", Efterrätt=" + dessert;
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
 
-    private void sendSignalToPersonalApp(Integer tableNr, Boolean starter, Boolean main, Boolean dessert) {
-        apiService.sendSignal(tableNr, starter, main, dessert).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                Toast.makeText(KitchenActivity.this, "Signal skickad till personalen för bord Nr " + tableNr, Toast.LENGTH_SHORT).show();
-            }
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-               Toast.makeText(KitchenActivity.this,
-                       "Fel vid skicka signal: " + t.getMessage(),
-                       Toast.LENGTH_SHORT).show();
-            }
-        });
+        // If you want to remove the order from the list, do it here:
+        // orderAdapter.removeOrder(order);
+
+        // Or if you want to call the backend to signal the staff:
+        // apiService.sendSignal(order.getTableNumber(), starter, main, dessert)...
     }
 }
--- app/src/main/java/com/dt170g/kitchen/OrderAdapter.java
+++ app/src/main/java/com/dt170g/kitchen/OrderAdapter.java
@@ -1,32 +1,24 @@
 package com.dt170g.kitchen;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
-
-import android.util.Log;
-
-import com.dt170g.api.ApiService;
-import com.dt170g.api.RetrofitClient;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import java.util.Map;
-import java.util.HashMap;
-
 
 public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
 
     private final List<RecievedOrder> orderList;
     private final OnOrderReadyListener listener;
 
+    // This interface notifies KitchenActivity when “Markera Order Som Klar” is pressed
     public interface OnOrderReadyListener {
         void onOrderReady(RecievedOrder order, Boolean starter, Boolean main, Boolean dessert);
     }
@@ -38,43 +30,68 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> 
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_order, parent, false);
+    public OrderAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_order, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull OrderAdapter.ViewHolder holder, int position) {
         RecievedOrder order = orderList.get(position);
+
+        // Set the table number
         holder.tableNumber.setText("Bord: " + order.getTableNumber());
 
+        // We'll build separate text for appetizer, main, and dessert
         StringBuilder appetizerList = new StringBuilder();
         StringBuilder mainCourseList = new StringBuilder();
         StringBuilder dessertList = new StringBuilder();
 
+        // If the order has orderSpecs, let's parse them
         if (order.getOrderSpecs() != null) {
             for (OrderSpecs dish : order.getOrderSpecs()) {
                 Log.d("ORDER_CHECK", "Rätt: " + dish.getMeal() + ", Kategori: " + dish.getCategory());
 
-                if ("Förrätt".equalsIgnoreCase(dish.getCategory())) {
-                    appetizerList.append(dish.getMeal()).append(" (").append(dish.getCount()).append(")\n");
-                } else if ("Huvudrätt".equalsIgnoreCase(dish.getCategory())) {
-                    mainCourseList.append(dish.getMeal()).append(" (").append(dish.getCount()).append(")\n");
-                } else if ("Efterrätt".equalsIgnoreCase(dish.getCategory())) {
-                    dessertList.append(dish.getMeal()).append(" (").append(dish.getCount()).append(")\n");
+                // We do a case-insensitive check for the category
+                String category = (dish.getCategory() != null) ? dish.getCategory().toLowerCase() : "";
+
+                if (category.contains("förrätt")) {
+                    appetizerList.append(dish.getMeal()).append(" (x").append(dish.getCount()).append(")\n");
+                }
+                else if (category.contains("huvudrätt") || category.contains("varmrätt")) {
+                    mainCourseList.append(dish.getMeal()).append(" (x").append(dish.getCount()).append(")\n");
+                }
+                else if (category.contains("efterrätt")) {
+                    dessertList.append(dish.getMeal()).append(" (x").append(dish.getCount()).append(")\n");
+                }
+                // If you have “Vegetariska” or other categories, handle them as you like
+                else {
+                    // optional: treat unknown categories or "Vegetariska"
+                    // mainCourseList.append(dish.getMeal()).append("... etc
                 }
             }
         }
 
-        holder.orderedAppetizer.setText(appetizerList.length() > 0 ? appetizerList.toString() : "Inga förrätter");
-        holder.orderedMainCourse.setText(mainCourseList.length() > 0 ? mainCourseList.toString() : "Inga huvudrätter");
-        holder.orderedDessert.setText(dessertList.length() > 0 ? dessertList.toString() : "Inga efterrätter");
+        // If nothing was added, show “Inga …”
+        holder.orderedAppetizer.setText(
+                appetizerList.length() > 0 ? appetizerList.toString() : "Inga förrätter"
+        );
+        holder.orderedMainCourse.setText(
+                mainCourseList.length() > 0 ? mainCourseList.toString() : "Inga huvudrätter"
+        );
+        holder.orderedDessert.setText(
+                dessertList.length() > 0 ? dessertList.toString() : "Inga efterrätter"
+        );
 
-        CheckBox starterChecked = holder.itemView.findViewById(R.id.starterCheckbox);
-        CheckBox mainChecked = holder.itemView.findViewById(R.id.mainCourseCheckbox);
-        CheckBox dessertChecked = holder.itemView.findViewById(R.id.dessertCheckbox);
+        // Set button click to notify the Activity
         holder.readyButton.setOnClickListener(v -> {
-            listener.onOrderReady(order, starterChecked.isChecked(), mainChecked.isChecked(), dessertChecked.isChecked());
+            boolean starterChecked = holder.starterCheckbox.isChecked();
+            boolean mainChecked = holder.mainCourseCheckbox.isChecked();
+            boolean dessertChecked = holder.dessertCheckbox.isChecked();
+
+            // Let the activity decide what to do
+            listener.onOrderReady(order, starterChecked, mainChecked, dessertChecked);
         });
     }
 
@@ -83,16 +100,33 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> 
         return orderList.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    // Optional: remove an order from the list and refresh
+    public void removeOrder(RecievedOrder order) {
+        int index = orderList.indexOf(order);
+        if (index != -1) {
+            orderList.remove(index);
+            notifyItemRemoved(index);
+        }
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        // Views from item_order.xml
         TextView tableNumber, orderedAppetizer, orderedMainCourse, orderedDessert;
+        CheckBox starterCheckbox, mainCourseCheckbox, dessertCheckbox;
         Button readyButton;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
             tableNumber = itemView.findViewById(R.id.tableNumber);
             orderedAppetizer = itemView.findViewById(R.id.orderedAppetizer);
             orderedMainCourse = itemView.findViewById(R.id.orderedMainCourse);
             orderedDessert = itemView.findViewById(R.id.orderedDessert);
+
+            starterCheckbox = itemView.findViewById(R.id.starterCheckbox);
+            mainCourseCheckbox = itemView.findViewById(R.id.mainCourseCheckbox);
+            dessertCheckbox = itemView.findViewById(R.id.dessertCheckbox);
+
             readyButton = itemView.findViewById(R.id.readyButton);
         }
     }
